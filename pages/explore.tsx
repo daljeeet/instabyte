166db90511b348f2906204f176a05194bc02dd3c
daljeet-coder <daljeetsingh.chamiyal@gmail.com>
2023-02-23 00:03:43 +0530

    updates in view Post

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -1,5 +1,5 @@
 import Navbar from '@/Components/Navbar'
-const  Post =require('../models/Post.js')
+import {Post} from  "@/models/Post.js"
 import dbConnect from '../lib/dbConnect'
 import React, { useEffect, useState } from 'react'
 import { postDataType } from '@/Components/CreateModal.jsx';
@@ -8,10 +8,10 @@ import PostDetails from '@/Components/PostDetails';
 import {elem} from '../Components/Card'
 import BlurImage from '@/Components/BlurImage';
 type dataTypes = {
-  data:postDataType
+  data:postDataType[]
 }
-const Explore = (data:dataTypes) => {
-  const posts = data.data;
+const Explore = ({data}:dataTypes) => {
+  const posts = data
   const [images, setImages ] = useState([""])
   const [modal, setModal] = useState(false)
   const [postObj,setPostObj] = useState(elem)
@@ -48,7 +48,7 @@ const closePostDtlModal = ()=>{
      <Navbar/>
     <div className='md:ml-52 pt-14 h-fit' >
       <div className='grid gap-4 p-4 md:grid-cols-3 grid-cols-1 sm:grid-cols-2 grid-rows-auto'>
-       {images?.map((el:string,id:number)=><Image key={id} placeholder='blur' blurDataURL={BlurImage} onClick={()=>handleImagePost(el)} src={el} alt='allpics' width={400} height={800} onLoad={(e:any)=>{e.target.naturalHeight>550?e.target.className="rounded-lg row-span-2 cursor-zoom-in ":e.target.className="rounded-lg self-center cursor-zoom-in"}} />
+       {images?.map((el:string,id:number)=><Image key={id} placeholder='blur' blurDataURL={BlurImage} onClick={()=>handleImagePost(el)} src={el||"/demo_img.png"} alt='allpics' width={400} height={800} onLoad={(e:any)=>{e.target.naturalHeight>550?e.target.className="rounded-lg row-span-2 cursor-zoom-in ":e.target.className="rounded-lg self-center cursor-zoom-in"}} />
         )}
     </div>
     </div>
@@ -58,9 +58,20 @@ const closePostDtlModal = ()=>{
 }
 export const getServerSideProps= async ()=>{
   await dbConnect()
-   const res = await Post.find().sort({_id:-1})
-   return { props: {
-    data:JSON.parse(JSON.stringify(res))
-   } }
+
+  let result =  await Post.aggregate([
+    {
+      $lookup: {
+          from: "users",
+          localField: "author",
+          foreignField: "id", 
+          as: "result"
+      }
+    }
+  ]).sort({_id: -1})
+//  const res = await Post.find().sort({_id:-1})
+      return { props: {data:JSON.parse(JSON.stringify(result))}}
+    // return { props: {data:JSON.parse(JSON.stringify(result))}}
+    
 }
 export default Explore;
