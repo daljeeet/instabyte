f88d1b6bdb707ff3e06d1cfb3a6a1ce1fd081b02
daljeet-coder <daljeetsingh.chamiyal@gmail.com>
2023-02-22 23:16:17 +0530

    made api for comments

---
+++ pages/api/protected/getcomments/[id].js
@@ -0,0 +1,19 @@
+import dbConnect from "../../../../lib/dbConnect"
+import {Comment} from "../../../../models/Comments"
+
+export default async function handler(req, res) {
+  await dbConnect()
+  const {id,method} = req;
+  switch (method) {
+    case 'GET':
+      try {
+        let allComments = Comment.find({parentId: id})
+        res.status(201).json({ success: true, data:allComments})
+      } catch (error) {
+        res.status(400).json({ success: false, error})
+      }
+      break
+    default:
+      res.status(400).json({ success: false,msg:`Cannot Find ${req.method}` })
+  }
+}
